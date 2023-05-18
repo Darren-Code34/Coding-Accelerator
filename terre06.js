ab17b1f371c79937a294a86be052722498c08ca0
Darren Mibissa <mibissadarren@outlook.com>
2023-05-18 16:18:59 +0100

    terre06

---
+++ terre06.js
@@ -0,0 +1,11 @@
+function inversion(chaine){
+    let tab = []
+    let reversed
+    for(i = chaine.length - 1; i >= 0; i--){
+        tab.push(chaine[i])
+    }
+    reversed = tab.join('')
+    console.log(reversed);
+}
+
+inversion("mot")
